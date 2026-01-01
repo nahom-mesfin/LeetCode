87ca12466b19f3766ec76d1333c4e8b2aae23404
nahom-mesfin <nahommesfin116@gmail.com>
2026-01-01 16:53:20 -0300

    Initial commit - add Two Sum problem

---
+++ Easy/1_Two_Sum.js
@@ -0,0 +1,44 @@
+/**
+ * January 1,2026
+ * Problem: Two Sum (Easy)
+ *
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ *
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Constraints:
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+  }
+};
